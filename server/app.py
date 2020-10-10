2ca561a5cb3be3e384cc7d43fa05fcdb2974068f
Abhishek Chaurasia <abhi24061997@gmail.com>
2020-10-10 17:58:48 +0530

    implemented websockets

--- server/app.py
+++ server/app.py
@@ -1,32 +1,41 @@
-# import the necessary packages
-from flask import Flask, render_template, Response
-from camera import Video
+from flask_socketio import SocketIO
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 
 @app.route('/')
 def index():
-    """Rendering the Homepage"""
+    """Home page."""
     return render_template('index.html')
 
 
-def gen(camera):
-    """Yields the Real-Time Video frames"""
-    while True:
-        # get camera frame
-        frame = camera.get_frame()
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+@socketio.on('connect', namespace='/web')
+def connect_web():
+    print('[INFO] Web client connected: {}'.format(request.sid))
 
 
-@app.route('/video_feed')
-def video_feed():
-    """Returns the video as a Response which is then rendered on the webpage"""
-    return Response(gen(Video()),
-                    mimetype='multipart/x-mixed-replace; boundary=frame')
+@socketio.on('disconnect', namespace='/web')
+def disconnect_web():
+    print('[INFO] Web client disconnected: {}'.format(request.sid))
 
 
-if __name__ == '__main__':
-    # defining server ip address and port
-    app.run(host='0.0.0.0', port='5000', debug=True)
+@socketio.on('connect', namespace='/cv')
+def connect_cv():
+    print('[INFO] CV client connected: {}'.format(request.sid))
+
+
+@socketio.on('disconnect', namespace='/cv')
+def disconnect_cv():
+    print('[INFO] CV client disconnected: {}'.format(request.sid))
+
+
+@socketio.on('cv2server')
+def handle_cv_message(message):
+    socketio.emit('server2web', message, namespace='/web')
+
+
+if __name__ == "__main__":
+    print('[INFO] Starting server at http://localhost:5001')
+    socketio.run(app=app, host='0.0.0.0', port=5001)
--- server/camera.py
+++ server/camera.py
@@ -1,9 +1,36 @@
 # import the necessary packages
+import base64
+import random
+
 import cv2
+# from flask_socketio import SocketIO
+import time
+from flask import Flask, render_template, request
+
+import socketio
+
+sio = socketio.Client()
+server_addr = "http://127.0.0.1:5001/"
+
+
+@sio.event
+def connect():
+    print('[INFO] Successfully connected to server.')
+
+
+@sio.event
+def connect_error():
+    print('[INFO] Failed to connect to server.')
+
+
+@sio.event
+def disconnect():
+    print('[INFO] Disconnected from server.')
 
 
 class Video(object):
     """This class process the Video received, processes it."""
+
     def __init__(self):
         # capturing video
         self.video = cv2.VideoCapture(0)
@@ -16,5 +43,35 @@ class Video(object):
         """Convert the video frames to bytes"""
         # extracting frames
         ret, frame = self.video.read()
+        cv2.rectangle(frame, (150, 200), (400, 400), (255, 0, 0), 2)
         ret, jpeg = cv2.imencode('.jpg', frame)
-        return jpeg.tobytes()
+        # cv2.rectangle(jpeg, (15, 15), (150, 150), (255, 0, 0), 7)
+        frame = base64.b64encode(jpeg).decode('utf-8')
+        return "data:image/jpeg;base64,{}".format(frame)
+
+
+def setup():
+    print("trying to connect to server")
+    sio.connect(
+        "http://0.0.0.0:5001/",
+        transports=['websocket'],
+        namespaces=['/cv'],
+    )
+    time.sleep(1)
+
+
+def main():
+    setup()
+    cam = Video()
+    words = ['Abhishek', 'Shubhank', 'Shreya']
+    while True:
+        sio.emit(
+            'cv2server',
+            {
+                'image': cam.get_frame(),
+                'text': random.choice(words)
+            })
+
+
+if __name__ == "__main__":
+    main()
